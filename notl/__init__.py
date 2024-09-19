9b4f004cc7dbd974c9b69ebcd88983e69d159f55
JakobEliasWagner <jakob@winter-wind.de>
2024-09-19 12:12:37 +0200

    add load run operator

--- notl/__init__.py
+++ notl/__init__.py
@@ -4,5 +4,6 @@ _extended_summary_.
 """
 
 from notl.encoded_fno import EncodedFourierNeuralOperator, Encoder, RFFEncoder, SineEncoder
+from notl.load_multirun import load_run_operator
 
-__all__ = ["RFFFourierNeuralOperator", "EncodedFourierNeuralOperator", "Encoder", "SineEncoder", "RFFEncoder"]
+__all__ = ["RFFFourierNeuralOperator", "EncodedFourierNeuralOperator", "Encoder", "SineEncoder", "RFFEncoder", "load_run_operator"]
